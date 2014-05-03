2912d3501c5097c0e01617b5098517efa1d17fe6
SionoiS <Simon-Pierre@Simon-Pierre-PC>
2014-05-03 15:15:42 -0400

    Mod loader stuff fixed

--- sionois/rusticammelius/CommonProxy.java
+++ sionois/rusticammelius/CommonProxy.java
@@ -9,7 +9,6 @@ import sionois.rusticammelius.Mobs.EntityWildCow;
 import sionois.rusticammelius.Mobs.EntityWildPig;
 import sionois.rusticammelius.Mobs.EntityWildSheep;
 import sionois.rusticammelius.Mobs.EntityWolfRM;
-import net.minecraft.src.ModLoader;
 import cpw.mods.fml.common.registry.EntityRegistry;
 
 public class CommonProxy
@@ -18,16 +17,16 @@ public class CommonProxy
 
 	public void registerTileEntities()
 	{
-		EntityRegistry.registerGlobalEntityID(EntityChickenRM.class, "ChickenRM", ModLoader.getUniqueEntityId(), 0xffffff, 0xaaaaaa);
-		EntityRegistry.registerGlobalEntityID(EntityCowRM.class, "CowRM", ModLoader.getUniqueEntityId(), 0xffffff, 0xaaaaaa);
-		EntityRegistry.registerGlobalEntityID(EntityPigRM.class, "PigRM", ModLoader.getUniqueEntityId(), 0xffffff, 0xaaaaaa);
-		EntityRegistry.registerGlobalEntityID(EntitySheepRM.class, "SheepRM", ModLoader.getUniqueEntityId(), 0xffffff, 0xaaaaaa);
-		EntityRegistry.registerGlobalEntityID(EntityWolfRM.class, "WolfRM", ModLoader.getUniqueEntityId(), 0xffffff, 0xaaaaaa);
+		EntityRegistry.registerGlobalEntityID(EntityChickenRM.class, "ChickenRM", EntityRegistry.findGlobalUniqueEntityId(), 0xffffff, 0xaaaaaa);
+		EntityRegistry.registerGlobalEntityID(EntityCowRM.class, "CowRM", EntityRegistry.findGlobalUniqueEntityId(), 0xffffff, 0xaaaaaa);
+		EntityRegistry.registerGlobalEntityID(EntityPigRM.class, "PigRM", EntityRegistry.findGlobalUniqueEntityId(), 0xffffff, 0xaaaaaa);
+		EntityRegistry.registerGlobalEntityID(EntitySheepRM.class, "SheepRM", EntityRegistry.findGlobalUniqueEntityId(), 0xffffff, 0xaaaaaa);
+		EntityRegistry.registerGlobalEntityID(EntityWolfRM.class, "WolfRM", EntityRegistry.findGlobalUniqueEntityId(), 0xffffff, 0xaaaaaa);
 		
-		EntityRegistry.registerGlobalEntityID(EntityWildChicken.class, "WildChicken", ModLoader.getUniqueEntityId(), 0xffffff, 0xaaaaaa);
-		EntityRegistry.registerGlobalEntityID(EntityWildCow.class, "WildCow", ModLoader.getUniqueEntityId(), 0xffffff, 0xaaaaaa);
-		EntityRegistry.registerGlobalEntityID(EntityWildPig.class, "WildPig", ModLoader.getUniqueEntityId(), 0xffffff, 0xaaaaaa);
-		EntityRegistry.registerGlobalEntityID(EntityWildSheep.class, "WildSheep", ModLoader.getUniqueEntityId(), 0xffffff, 0xaaaaaa);
+		EntityRegistry.registerGlobalEntityID(EntityWildChicken.class, "WildChicken", EntityRegistry.findGlobalUniqueEntityId(), 0xffffff, 0xaaaaaa);
+		EntityRegistry.registerGlobalEntityID(EntityWildCow.class, "WildCow", EntityRegistry.findGlobalUniqueEntityId(), 0xffffff, 0xaaaaaa);
+		EntityRegistry.registerGlobalEntityID(EntityWildPig.class, "WildPig", EntityRegistry.findGlobalUniqueEntityId(), 0xffffff, 0xaaaaaa);
+		EntityRegistry.registerGlobalEntityID(EntityWildSheep.class, "WildSheep", EntityRegistry.findGlobalUniqueEntityId(), 0xffffff, 0xaaaaaa);
 	}
 
 }
--- sionois/rusticammelius/RMBlocks.java
+++ sionois/rusticammelius/RMBlocks.java
@@ -1,12 +1,5 @@
 package sionois.rusticammelius;
 
-import java.util.Arrays;
-import java.util.HashSet;
-import java.util.Set;
-
-import net.minecraft.block.Block;
-
-
 public class RMBlocks
 {
 	   
